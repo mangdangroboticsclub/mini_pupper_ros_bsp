e00d0695e6efbcf2b7b7693516027cea89fbccd0
Horst Dumcke <hdumcke@marlysys.com>
2023-05-27 15:46:12 +0200

    changed orientation of Lidar

--- mini_pupper_bringup/launch/ld06.launch.py
+++ mini_pupper_bringup/launch/ld06.launch.py
@@ -47,7 +47,7 @@ def generate_launch_description():
         package='tf2_ros',
         executable='static_transform_publisher',
         name='base_link_to_base_laser_ld06',
-        arguments=['0', '0', '0', '-1.5708', '0', '0', 'base_link', 'base_laser']
+        arguments=['0', '0', '0', '1.5708', '0', '0', 'base_link', 'base_laser']
     )
 
     # Define LaunchDescription variable
